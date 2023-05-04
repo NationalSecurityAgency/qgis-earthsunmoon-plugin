3dc270e0016561b321c7d56407b171825ad8e6b4
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-05-04 13:57:07 +0000

    Fix to allow qgis_process to function correctly

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
 
 def classFactory(iface):
-    from .earthsunmoon import EarthSunMoon
-    return EarthSunMoon(iface)
+    if iface:
+        from .earthsunmoon import EarthSunMoon
+        return EarthSunMoon(iface)
+    else:
+        # This is used when the plugin is loaded from the command line command qgis_process
+        from .earthsunmoonprocessing import EarthSunMoon
+        return EarthSunMoon(iface)
---
+++ earthsunmoonprocessing.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from qgis.core import QgsApplication
+# Check to see if the TimeZoneFinder and Skyfield libraries are installed
+try:
+    from timezonefinder import TimezoneFinder
+    from skyfield.api import wgs84
+    from jplephem.spk import SPK
+    libraries_found = True
+    from .provider import EarthSunMoonProvider
+except Exception:
+    # traceback.print_exc()
+    libraries_found = False
+
+class EarthSunMoon(object):
+    solarInfoDialog = None
+    ephemInfoDialog = None
+    settingsDialog = None
+
+    def __init__(self):
+        self.provider = None
+
+    def initProcessing(self):
+        if libraries_found:
+            self.provider = EarthSunMoonProvider()
+            QgsApplication.processingRegistry().addProvider(self.provider)
+
+    def initGui(self):
+        self.initProcessing()
+
+    def unload(self):
+        if self.provider:
+            QgsApplication.processingRegistry().removeProvider(self.provider)
